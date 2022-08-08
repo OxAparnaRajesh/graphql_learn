33a3fd5660975e95fe87410b94bfd7bd36641c32
MasterAtWork <54503164+iamaparnarajesh@users.noreply.github.com>
2022-08-08 13:06:23 +0800

    mongodb schema created

--- server/app.js
+++ server/app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require('./schema/schema');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+//connect to mlab database
+
+mongoose.connect("mongodb+srv://iamaparnarajesh:jUejy00uISjeYKdL@cluster0.lhvd983.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connection.once('open',() => {
+    console.log("mongodb connected");
+})
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
